84ca4db54d1895844b19459f0c2fbf331d964fc5
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-05-08 21:35:29 -0300

    update

---
+++ src/components/shared/PostStats.tsx
@@ -0,0 +1,21 @@
+import { Models } from "appwrite";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import { checkIsLiked } from "@/lib/utils";
+import {
+  useLikePost,
+  useSavePost,
+  useDeleteSavedPost,
+  useGetCurrentUser,
+} from "@/lib/react-query/queries";
+
+type PostStatsProps = {
+  post: Models.Document;
+  userId: string;
+};
+
+const PostStats = ({ post, userId }: PostStatsProps) => {
+  const location = useLocation();
+  const likesList = post.likes.map((user: Models.Document) => user.$id);
+};
---
+++ src/components/shared/ProfileUploader.tsx
@@ -0,0 +1,9 @@
+import { useCallback, useState } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+
+import { convertFileToUrl } from "@/lib/utils";
+
+type ProfileUploaderProps = {
+  fieldChange: (files: File[]) => void;
+  mediaUrl: string;
+};
